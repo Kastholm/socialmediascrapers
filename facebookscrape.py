bcb3c01c2549d8d42dab0eb1ce07b6198e484d7a
Marc <marc@aguardio.com>
2024-11-18 11:29:49 +0100

    facebook test

--- facebookscrape.py
+++ facebookscrape.py
@@ -3,6 +3,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
+from lxml import etree
 import requests
 import time
 import json
@@ -12,7 +13,7 @@ from datetime import date
 chromedriver_bin = './chromedriver/chromedriver-windows/chromedriver.exe'
 url ='https://www.facebook.com/3Danmark'
 today = date.today()
-index = 0
+index = 1
 
 # Opret ChromeService med stien til ChromeDriver
 service = webdriver.ChromeService(executable_path=chromedriver_bin)
@@ -20,153 +21,259 @@ driver = webdriver.Chrome(service=service)
 
 #Open Tiktok
 driver.get(url)
+
+
+
 # Login
 time.sleep(2)
-button = driver.find_element(By.XPATH, "//span[text()='Tillad alle cookies']")
+button = driver.find_element(By.XPATH, '/html/body/div[2]/div[1]/div/div[2]/div/div/div/div/div[2]/div/div[2]/div[1]/div')
 button.click()
 
-time.sleep(2)
+button = driver.find_element(By.XPATH, '/html/body')
+button.click()
 
-input_field = driver.find_element(By.NAME, 'email')
+
+input_field = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[1]/div/div[5]/div/div/div[1]/div/div[2]/div/div/div/div[2]/form/div/div[3]/div/div/label/div/input')
 input_field.send_keys('mac@mgdk.dk')
 time.sleep(2)
-input_field = driver.find_element(By.NAME, 'pass')
+input_field = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[1]/div/div[5]/div/div/div[1]/div/div[2]/div/div/div/div[2]/form/div/div[4]/div/div/label/div/input')
 input_field.send_keys('')
 
 time.sleep(1)
 
-button = driver.find_element(By.CSS_SELECTOR, '//div[aria-label="Accessible login button"]')
+button = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[1]/div/div[5]/div/div/div[1]/div/div[2]/div/div/div/div[2]/form/div/div[5]/div')
 button.click()
 
-time.sleep(30)
+#time.sleep(15)
+#driver.execute_script("document.body.style.zoom='20%'")
 
-# Start scrolling down for 5 seconds
-#end_time = time.time() + 10
-#while time.time() < end_time:
- #   driver.find_element("tag name", "body").send_keys(Keys.PAGE_DOWN)
-  #  time.sleep(0.1)  
-time.sleep(30)
-driver.execute_script("document.body.style.zoom='20%'")
-time.sleep(30)
+time.sleep(10)
 soup = BeautifulSoup(driver.page_source, 'html.parser')
-posts = driver.find_elements(By.CSS_SELECTOR, '/html/body/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div[2]/div/div/div[4]/div/div/div/div/div[1]/div/div[2]/div[2]/span/div/span/span')
-videoAmount = soup.select('/html/body/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div[2]/div/div/div[4]/div/div/div/div/div[1]/div/div[2]/div[2]/span/div/span/span')
-print(len(videoAmount))
+#postEle = driver.find_elements(By.XPATH, '/html/body/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div[2]/div/div/div[4]/div/div/div/div/div[1]/div/div[2]/div[2]/span/div/span/span')
+#posts = []  # Tom liste til at gemme elementer, der ikke har '1 kommentar'
+#for ele in postEle:
+#    if ele.text != '1 kommentar':
+#        posts.append(ele)
+#dom = etree.HTML(str(soup))
+#videoAmount = dom.xpath('/html/body/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div[2]/div/div/div[4]/div/div/div/div/div[1]/div/div[2]/div[2]/span/div/span/span')
+#print(len(videoAmount))
 
-
-while index <= len(videoAmount) :
+while index <= 300 :
     #Click post
-    time.sleep(5)
-    if posts:
-        print(len(posts))
-        print('index', index)
-        posts[index].click()
-    #time.sleep(3)
-    #soup = BeautifulSoup(driver.page_source, 'html.parser')
-    #legit = soup.select_one('h2._a9zc:has(a[href="/3danmark/"])')
-    #if not legit:
-    #    index += 1
-    #    close = driver.find_element(By.CSS_SELECTOR, 'div.x160vmok:has(svg)')
-    #    close.click()
-    #    time.sleep(2)
-    #    continue
+    #time.sleep(5000000)
+    #if posts:
+    #    print(len(posts))
+    #    print('index', index)
+    #    posts[index].click()
 
     time.sleep(10)
     # Start scrolling down for 5 seconds
-    end_time = time.time() + 10
-    while time.time() < end_time:
-       driver.find_element("/html/body/div[1]/div/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div").send_keys(Keys.PAGE_DOWN)
-       time.sleep(0.1)  
 
     #Scrape page
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     time.sleep(2)
 
+    #ReadMore?
+    try:
+        readMoreTitle = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[1]/div/div/div/div/div/div/div/div/div/div/div[2]/div/div/div[3]/div[1]/div/div/div/div/span/div[3]/div[2]/div')
+        readMoreTitle.click()
+    except Exception as e:
+        print("Elementet blev ikke fundet:")
+
     #title
-    title = soup.find('/html/body/div[1]/div/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[1]/div/div/div/div/div/div/div/div/div/div/div[2]/div/div/div[3]/div[1]/div/div/div/div/span').text
-    print(title)
+    dom = etree.HTML(str(soup))
+    try: 
+        titleEle = dom.xpath('/html/body/div[1]/div/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div/div[2]/div[2]/div/div/div[1]/div/div/div/div/div/div/div/div/div/div/div[2]/div/div/div[3]/div[1]/div/div/div/div/span')
+        title = titleEle[0].text
+        print(title)
+    except:
+        title = 'Ingen titel på opslaget'
+        print("No titel")
+
     #published
-    published = soup.select_one('/html/body/div[1]/div/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[1]/div/div/div/div/div/div/div/div/div/div/div[2]/div/div/div[2]/div/div[2]/div/div[2]/span/div/span[1]/span/span/a')
-    print(published)
-    #url
-    postUrl = driver.current_url
-    print(postUrl)
+    try:
+        publishedEle = dom.xpath('/html/body/div[1]/div/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[1]/div/div/div/div/div/div/div/div/div/div/div[2]/div/div/div[2]/div/div[2]/div/div[2]/span/div/span[1]/span/span/a')
+        published = publishedEle[0].text
+    except:
+        published = 'Ingen udgivelsesdato'
+        print('Ingen udgivelsesdato')
+
+    #image url
+    try:
+        imgEle = dom.xpath('/html/body/div[1]/div/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[1]/div/div/div/div/div/div/div/div/div/div/div[2]/div/div/div[3]/div/div[1]/a/div[1]/div/div/div/img')
+        postUrl = imgEle[0].get('src')
+    except:
+        postUrl = 'intet billede'
+        print('No Image')
+
+    #likes
+    try:
+        likesEle = dom.xpath('/html/body/div[1]/div/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/div/div[1]/div/span/div/span[2]/span/span')
+        likes = likesEle[0].text
+    except:
+        likes = 'Ingen likes defineret'
+        print('ingen likes')
+
+    #shares
+    try:
+        sharesEle = dom.xpath('/html/body/div[1]/div/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/div/div[2]/div[2]/span/div/span/span')
+        shares = sharesEle[0].text
+    except:
+        shares = 'Ingen shares defineret'
+        print('ingen shares')
+
+    #comments
+    try:
+        commentsEle = dom.xpath('/html/body/div[1]/div/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/div/div[2]/div[1]/span/div/span/span')
+        comments = commentsEle[0].text
+    except:
+        shares = 'Ingen comments defineret'
+        print('ingen comments')
+
     #start JSON doc
     post = {
         "videoTitle": f"{title}",
         "url": f"{postUrl}",
         "published": f"{published}",
         "dataFetchDate": f"{str(today)}",
+        "likes": f"{likes}",
+        "shared": f"{shares}",
+        "commentsAmount": f"{comments}",
         "comments": []
     }
-    #Look for reply click events
-    time.sleep(4)
-    #article button with classes _acan _acao _acas _aj1- _ap30 which has a div and a span
-    reply_sections = driver.find_elements(By.CSS_SELECTOR, '/html/body/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div[2]/div/div/div[2]/div[2]/span/span/div/div[4]')
-# alle svar /html/body/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div[2]/div[1]/div/div[2]/div/div[3]/div[2]    
-    if reply_sections:
-        for replyEvent in reply_sections:
-            replyEvent.send_keys(Keys.ENTER)
-    time.sleep(15)
+
+    print(post)
+
+    #Find Repliy buttons
+    try:
+        reply_sections = driver.find_elements(By.XPATH, '/html/body/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div[2]/div/div/div[2]/div[2]/span/span/div/div[4]')
+        if reply_sections:
+            for replyEvent in reply_sections:
+                replyEvent.click()
+    except:
+        print('Ingen replies i denne post')
+
+    time.sleep(8)
+
+    # Find Replies replies buttons
+    #try:
+    #    reply_reply_sections = driver.find.elements(By.XPATH, '/html/body/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div[2]/div[1]/div/div[2]/div/div[3]/div[2]/span')
+    #    if reply_reply_sections:
+    #        for replyEvent in reply_reply_sections:
+    #            replyEvent.click()
+    #except:
+    #    print('Ingen reply replies i denne post')
 
     # Opdater soup med det nye page_source
     soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-    #select all comments
-    
-    comment_sections = soup.select('ul._a9ym')
+
+    #Find comment sections
+    # Find comment sections
+    comment_sections = dom.xpath('/html/body/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div[3]/div/div')
     for section in comment_sections:
-        #For hver kommentar henter vi 3 stykker information:
-        user = section.select_one('ul._a9ym > div > li._a9zj h3 > div > span > div > a')
-        user = user.text if user else 'N/A' 
+        # Username
+        try:
+            userEle = section.xpath('.//span/a/span/span')  # Brug section.xpath() for relative søgninger
+            user = userEle[0].text if userEle else 'Intet brugernavn fundet'
+        except:
+            user = 'Intet brugernavn fundet'
+            print('no username One')
 
-        comment = section.select_one('ul._a9ym > div > li._a9zj div._a9zs span')
-        comment = comment.text if comment else 'N/A' 
+        # Comment
+        try:
+            commentEle = section.xpath('.//div/span/div')  # Gør det relativt til section
+            comment = " ".join([ele.text for ele in commentEle if ele.text is not None]) if commentEle else 'Ingen kommentar fundet'
+        except:
+            comment = 'Ingen kommentar fundet'
+            print('no user comment')
 
-        timestampDate = section.select_one('ul._a9ym > div > li._a9zj time[datetime]')
-        timestamp = timestampDate['datetime'].split("T")[0] if timestampDate else 'N/A'
+        # Likes
+        try:
+            likesEle = section.xpath('.//span/div/div[1]/span')  # Gør det relativt til section
+            likes = likesEle[0].text if likesEle else 'Ingen likes'
+        except:
+            likes = 'Ingen likes'
+            print('no likes')
 
-        print(section)
-
-        replies = section.select('ul._a9ym > li > ul > div')
         reply_data = []
-         # Iterer over alle replies og hent deres data
+        replies = section.xpath('.//div[2]/div[2]/div[1]/div[1]')  # Gør det relativt til section
+
+        # Replies
         if replies:
             for reply in replies:
-                # Hent reply_user med fallback
-                reply_user_element = reply.select_one('ul._a9ym > li > ul > div h3')
-                reply_user = reply_user_element.text if reply_user_element else 'N/A'
+                # Username
+                try:
+                    reply_userEle = reply.xpath('.//span[1]/a/span/span')  # Gør det relativt til reply
+                    reply_user = reply_userEle[0].text if reply_userEle else 'Intet brugernavn fundet'
+                except:
+                    reply_user = 'Intet brugernavn fundet'
+                    print('no username Two')
 
-                # Hent reply_comment med fallback
-                reply_comment_element = reply.select_one('ul._a9ym > li > ul > div span._ap3a')
-                reply_comment = reply_comment_element.text if reply_comment_element else 'N/A'
+                # Comment
+                try:
+                    reply_commentEle = reply.xpath('.//div[2]/span/div/div')  # Gør det relativt til reply
+                    reply_comment = reply_commentEle[0].text if reply_commentEle else 'Ingen kommentar fundet'
+                except:
+                    reply_comment = 'Ingen kommentar fundet'
+                    print('no user comment')
 
-                reply_timestamp_element = reply.select_one('ul._a9ym > li > ul > div span time[datetime]')
-                reply_timestamp = (reply_timestamp_element['datetime'].split("T")[0] if reply_timestamp_element else 'N/A')
+                # Likes
+                try:
+                    reply_likesEle = reply.xpath('.//div[2]/div[2]/div/span/div/div[1]/span')  # Gør det relativt til reply
+                    reply_likes = reply_likesEle[0].text if reply_likesEle else 'Ingen likes'
+                except:
+                    reply_likes = 'Ingen likes'
+                    print('no likes')
+
+                # Reply replies
+                reply_reply_data = []
+                reply_replies = reply.xpath('.//div[1]/div[1]')  # Gør det relativt til reply
+                if reply_replies:
+                    for reply_reply in reply_replies:
+                        try:
+                            reply_reply_userEle = reply_reply.xpath('.//span/a/span/span')  # Gør det relativt til reply_reply
+                            reply_reply_user = reply_reply_userEle[0].text if reply_reply_userEle else 'Intet brugernavn fundet'
+                        except:
+                            reply_reply_user = 'Intet brugernavn fundet'
+                            print('no username Two')
+
+                        # Comment
+                        try:
+                            reply_reply_commentEle = reply_reply.xpath('.//span/div/div')  # Gør det relativt til reply_reply
+                            reply_reply_comment = reply_reply_commentEle[0].text if reply_reply_commentEle else 'Ingen kommentar fundet'
+                        except:
+                            reply_reply_comment = 'Ingen kommentar fundet'
+                            print('no comment found')
+
+                        reply_reply_data.append({
+                            "user": reply_reply_user,
+                            "comment": reply_reply_comment,
+                        })
+
                 # Tilføj reply data til listen
                 reply_data.append({
                     "user": reply_user,
                     "comment": reply_comment,
-                    "timestamp": reply_timestamp
+                    "likes": reply_likes,
+                    "replies": reply_reply_data
                 })
 
         # Tilføj den oprindelige kommentar og dens replies som ét objekt til data
         post['comments'].append({
             "user": user,
             "comment": comment,
-            "timestamp": timestamp,
-            "replies": reply_data  # Liste med alle svar til denne kommentar
+            "likes": likes,
+            "replies": reply_data
         })
+
 
     # Konverter til JSON
     json_data = json.dumps(post, ensure_ascii=False, indent=4)
-    print(json_data)
     # Lav JSON doc
-    with open(f"insta_scrape{index}.json", "w", encoding='utf-8') as outfile:
+    with open(f"facebook_scrape{index}.json", "w", encoding='utf-8') as outfile:
         outfile.write(json_data)
     index += 1
-    close = driver.find_element(By.CSS_SELECTOR, 'div.x160vmok:has(svg)')
+    close = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div[1]/div/div[3]/div')
     close.click()
-    time.sleep(4)
-
-
+    time.sleep(30)
